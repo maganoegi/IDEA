e14fd42b240dfb4745be226effe16d18e1e3c98c
maganoegi <platserg94@yahoo.com>
2020-01-04 22:59:59 +0100

    screw this

--- decrypt.py
+++ decrypt.py
@@ -9,22 +9,23 @@ def decrypt(block, key):
     X = split_into_x_parts_of_y(binaryData, 4, 16)
     # TODO: fix the return - needs to be 128 but not just a multiple of 8
     pre_subkeys = generate_subkeys(key)
-    subkeys = calculate_inv_of_keys(pre_subkeys)
+    subkeys = generate_decrypt_keys(pre_subkeys)
     result = []
 
-    for i in range(9):
+    for i in range(8):
     # variable names will follow the fourteen-step method 
     # described in the document
         Z = subkeys[i]
-        one = m_multiplication(X[0], Z[0])
+
+        one = m_mul(X[0], Z[0])
         two = m_sum(X[1], Z[1])
         three = m_sum(X[2], Z[2])
-        four = m_multiplication(X[3], Z[3])
+        four = m_mul(X[3], Z[3])
         five = XOR(one, three)
         six = XOR(two, four)
-        seven = m_multiplication(five, Z[4])
+        seven = m_mul(five, Z[4])
         eight = m_sum(six, seven)
-        nine = m_multiplication(eight, Z[5])
+        nine = m_mul(eight, Z[5])
         ten = m_sum(seven, nine)
         eleven = XOR(one, nine)
         twelve = XOR(three, nine)
@@ -41,10 +42,13 @@ def decrypt(block, key):
 
 if __name__ == '__main__':
 
-    data = "1100101110010101110111000001000111110011101010110100000100001000"
-    key = "10011011111000101001111111010111010011001001110010000101000101101001001001111010111010010010111000010111101101010101010101101110"
+    originaldata="00010100110010000010100110010000010100110010000001100111111010"
+    encrypteddata="1111001110000100111110101100111111011101010111001000010011110001"
+    key = "00000000011001000000000011001000000000010010110000000001100100000000000111110100000000100101100000000010101111000000001100100000"
 
-    encrypted_data = decrypt(data)
 
-    print(encrypted_data)
+
+    result = decrypt(encrypteddata, key)
+    print("result = " + result)
+    print("original = " + originaldata)
 
--- lib.py
+++ lib.py
@@ -9,20 +9,9 @@ def readFile(filename):
         return file.read().replace('\n', ' ')
 
 
-def AND(a, b):
-    if len(a) != len(b):
-        raise Exception('AND operator unequal sizes between inputs') 
-
-    result = ""
-    for i in range(len(a)):
-        result += '1' if a[i] == b[i] == '1' else '0'
-    
-    return result
-
-
 def XOR(a, b):
     if len(a) != len(b):
-        raise Exception('XOR operator unequal sizes between inputs') 
+        raise Exception('XOR operator unequal sizes between inputs a={} b={}'.format(len(a), len(b))) 
 
     result = ""
     for i in range(len(a)):
@@ -44,10 +33,34 @@ def generate_subkeys(data):
         raise Exception('generate keys function requires an input of 128 bits, but received {}'.format(len(data)))
     
     keys = []
-    for i in range(9):
-        subkeys = split_into_x_parts_of_y(data, 6, 16)
-        keys.append(subkeys)
+    for i in range(8):
+        subkeys = split_into_x_parts_of_y(data, 8, 16)
+        keys.append(subkeys[:-2])
         data = circular_left_shift(data, 25)
+
+
+    # subkeys = split_into_x_parts_of_y(data, 8, 16)
+    # active = subkeys[:-2]
+    # reserve = subkeys[-2:]
+    # keys = [active]
+    # data = circular_left_shift(data, 25)
+
+    # for i in range(1, 9):
+
+    #     subkeys = split_into_x_parts_of_y(data, 8, 16)
+    #     active = reserve + subkeys
+    #     reserve = active[-2:]
+    #     active = active[:-2]
+
+    #     data = ""
+    #     for j in range(len(active)):
+    #         data += active[j]
+    #     data = circular_left_shift(data, 25)
+
+
+    #     keys.append(active)
+
+    
     return keys
 
 
@@ -62,16 +75,62 @@ def split_into_x_parts_of_y(data, x, y):
 
 
 # source: https://eprint.iacr.org/2014/704.pdf page 3,4
-def calculate_inv_of_keys(keys):
-    pass
+def generate_decrypt_keys(keys):
+    decrypt_keys = []
+    for i in range(len(keys)):
+        row = [
+            int2bits(m_mul_inv(keys[7-i][0], two_sixteen_plus_1)),
+            int2bits(m_sum_inv(keys[7-i][1], two_sixteen)),
+            int2bits(m_sum_inv(keys[7-i][2], two_sixteen)),
+            int2bits(m_mul_inv(keys[7-i][3], two_sixteen_plus_1)),
+            keys[7-i][4],
+            keys[7-i][5]
+        ]
+        decrypt_keys.append(row)
+    return decrypt_keys
 
 
-def m_multiplication(a, b):
+
+# source: https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
+def m_mul_inv(a, m):
+    m0 = m 
+    y = 0
+    x = 1
+    a = int(a, 2)
+    if (m == 1) : 
+        return 0
+
+    while (a > 1) : 
+
+        # q is quotient 
+        q = a // m 
+        t = m 
+
+        # m is remainder now, process 
+        # same as Euclid's algo 
+        m = a % m 
+        a = t 
+        t = y 
+
+        # Update x and y 
+        y = x - q * y 
+        x = t 
+
+    # Make x positive 
+    if (x < 0) : 
+        x = x + m0 
+
+    return x 
+
+def m_sum_inv(a, m):
+    return m - int(a, 2)
+
+def m_mul(a, b):
     a = int(a, 2) 
     b = int(b, 2)
     res = (a * b) % two_sixteen_plus_1
     bits = bin(res)[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
+    return bits.zfill(16)
 
 
 def m_sum(a, b):
@@ -79,12 +138,12 @@ def m_sum(a, b):
     b = int(b, 2)
     res = (a + b) % two_sixteen
     bits = bin(res)[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
+    return bits.zfill(16)
 
 
 def int2bits(val):
     bits = bin(val)[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
+    return bits.zfill(16)
 
 
 # source: https://stackoverflow.com/questions/7396849/convert-binary-to-ascii-and-vice-versa
@@ -102,16 +161,16 @@ def int2bytes(i):
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
 # source: https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+# def egcd(a, b):
+#     if a == 0:
+#         return (b, 0, 1)
+#     else:
+#         g, y, x = egcd(b % a, a)
+#         return (g, x - (b // a) * y, y)
 
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
+# def modinv(a, m):
+#     g, x, y = egcd(a, m)
+#     if g != 1:
+#         raise Exception('modular inverse does not exist')
+#     else:
+#         return x % m
